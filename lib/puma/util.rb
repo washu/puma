8cf5deb4f959e9dc6f5f39f338caf435f675c3d6
Sal Scotto <sal.scotto@gmail.com>
2013-12-26 15:38:07 -0500

    fixup

--- lib/puma/util.rb
+++ lib/puma/util.rb
@@ -1,11 +1,11 @@
+require 'fcntl'
 module Puma
   module Util
     module_function
-
     def pipe
       a,b = IO.pipe
       a_flag = a.fcntl(Fcntl::F_GETFD, 0)
-      b_glag = b.fcntl(Fcntl::F_GETFD, 0)
+      b_flag = b.fcntl(Fcntl::F_GETFD, 0)
       a.fcntl(Fcntl::F_SETFD, a_flag | Fcntl::FD_CLOEXEC)
       b.fcntl(Fcntl::F_SETFD, b_flag | Fcntl::FD_CLOEXEC)
       a,b
